83593dda6c11de4fb1cc8d53fe5ae3984d44fd1c
sangT220222 <magnas9@hotmail.co.uk>
2024-03-14 23:25:48 +0000

    code clean up and added functions to handle logic

--- Day 1/script.js
+++ Day 1/script.js
@@ -1,4 +1,8 @@
-const letter_a = (keyCode) => { return document.querySelector(`div[data-key="${keyCode}"].key`) }
+//get div
+const get_selected_div = (keyCode) => { return document.querySelector(`div[data-key="${keyCode}"]`) }
+ 
+//get audio
+const get_selected_audio = (keyCode) => { return document.querySelector(`audio[data-key="${keyCode}"]`) }
  
 //add playing to classList
 const add_playing_to_class_list = (div_to_add) => { div_to_add.classList.add('playing')}
@@ -6,78 +10,17 @@ const add_playing_to_class_list = (div_to_add) => { div_to_add.classList.add('pl
 //remove playing from classList
 const remove_playing_from_class_list = (div_to_remove_from) => { div_to_remove_from.classList.remove('playing')}
  
-//get audio
- 
-// document.addEventListener("keydown", (event) => {
-//   if (event.key === "a"){
-//     //want to add class name of "playing"
-//     letter_a().classList.add('playing');
-//   }
-//   else if (event.key === "s"){
-//     letter_s().classList.add('playing');
-//   }
-//   else if (event.key === "d"){
-//     letter_d().classList.add('playing');
-//   }
-//   else if (event.key === "f"){
-//     letter_f().classList.add('playing');
-//   }
-//   else if (event.key === "g"){
-//     letter_g().classList.add('playing');
-//   }
-//   else if (event.key === "h"){
-//     letter_h().classList.add('playing');
-//   }
-//   else if (event.key === "j"){
-//     letter_j().classList.add('playing');
-//   }
-//   else if (event.key === "k"){
-//     letter_k().classList.add('playing');
-//   }
-//   else if (event.key === "l"){
-//     letter_l().classList.add('playing');
-//   }
-// })
- 
- 
-// document.addEventListener("keyup", (event) => {
-//   if(event.key === "a"){
-//     //remove class name of "playing"
-//     letter_a().classList.remove('playing');
-//   }
-//   else if (event.key === "s"){
-//     letter_s().classList.remove('playing');
-//   }
-//   else if (event.key === "d"){
-//     letter_d().classList.remove('playing');
-//   }
-//   else if (event.key === "f"){
-//     letter_f().classList.remove('playing');
-//   }
-//   else if (event.key === "g"){
-//     letter_g().classList.remove('playing');
-//   }
-//   else if (event.key === "h"){
-//     letter_h().classList.remove('playing');
-//   }
-//   else if (event.key === "j"){
-//     letter_j().classList.remove('playing');
-//   }
-//   else if (event.key === "k"){
-//     letter_k().classList.remove('playing');
-//   }
-//   else if (event.key === "l"){
-//     letter_l().classList.remove('playing');
-//   }
-// })
  
  
 document.addEventListener("keydown", (event) => {
-  const get_div = letter_a(event.which);
+  const get_div = get_selected_div(event.which);
+  const get_audio = get_selected_audio(event.which);
   add_playing_to_class_list(get_div);
+  get_audio.currentTime = 0;
+  get_audio.play();
 })
  
 document.addEventListener("keyup", (event) => {
-  const get_div = letter_a(event.which);
+  const get_div = get_selected_div(event.which);
   remove_playing_from_class_list(get_div);
 })
