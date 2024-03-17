d49bd72df5b036293e9cbc9c5c8b5c7463fbcb20
sangT220222 <magnas9@hotmail.co.uk>
2024-03-17 10:07:50 +0000

    added functions that will handle if event is changed on the website

--- Day 3/script.js
+++ Day 3/script.js
@@ -1,1 +1,9 @@
-console.log("Test");
+const inputs = document.querySelectorAll('.controls input');
+//querySelector gives you an array, where ofc we can use the array methods to loop etc
+
+
+function handle_update(){
+    console.log(this.value);
+}
+
+inputs.forEach(input => input.addEventListener('change',handle_update))
