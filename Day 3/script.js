d55f358b83754b73bb43d4688b075b28b70d9a79
sangT220222 <magnas9@hotmail.co.uk>
2024-03-17 16:45:45 +0000

    updates so that when once changes value of spacing and blur, it will apply the change through JS

--- Day 3/script.js
+++ Day 3/script.js
@@ -1,9 +1,14 @@
 const inputs = document.querySelectorAll('.controls input');
 //querySelector gives you an array, where ofc we can use the array methods to loop etc
-
+const image = document.querySelector('img')
 
 function handle_update(){
-    console.log(this.value);
+    const unit = this.getAttribute('unit') || '';
+    // console.log(this.value)
+    // console.log(unit);
+    document.documentElement.style.setProperty(`--${this.name}`,this.value + unit)
 }
 
+//this will be when we change the range of blur and spacing of the image
 inputs.forEach(input => input.addEventListener('change',handle_update))
+
