d41830eb319720b494b08ea0f0358111b484835a
sangT220222 <magnas9@hotmail.co.uk>
2024-03-19 20:28:45 +0000

    final code added to add in the JS listeners to achieve desired output

--- Day 5/script.js
+++ Day 5/script.js
@@ -1,1 +1,13 @@
-console.log("TEST")
+//grabbing the panels
+const panels = document.querySelectorAll('.panel');
+
+function toggle_open () {
+    this.classList.toggle('open');
+}
+
+function toggle_transition () {
+    this.classList.toggle('transition_in');
+}
+
+panels.forEach(panel => panel.addEventListener('click',toggle_open)); //toggle_open() will be called when page loads
+panels.forEach(panel => panel.addEventListener('click',toggle_transition));
