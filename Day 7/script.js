dc129cd9d145a7a14207e41044625c01fd879c40
Sangam <100322380+sangT220222@users.noreply.github.com>
2024-03-20 09:32:21 +0000

    Day 7 of the JS30 practice

---
+++ Day 7/script.js
@@ -0,0 +1,38 @@
+// ## Array Cardio Day 2
+
+const people = [
+  { name: 'Wes', year: 1988 },
+  { name: 'Kait', year: 1986 },
+  { name: 'Irv', year: 1970 },
+  { name: 'Lux', year: 2015 }
+];
+
+const comments = [
+  { text: 'Love this!', id: 523423 },
+  { text: 'Super good', id: 823423 },
+  { text: 'You are the best', id: 2039842 },
+  { text: 'Ramen is my fav food ever', id: 123523 },
+  { text: 'Nice Nice Nice!', id: 542328 }
+];
+
+// Some and Every Checks
+// Array.prototype.some() // is at least one person 19 or older?
+const check = people.some(person => ((new Date().getFullYear()) - person.year >= 19));
+console.log(check);
+
+// Array.prototype.every() // is everyone 19 or older?
+const check_every = people.every(person=> ((new Date().getFullYear) - person.year >= 19));
+console.log(check_every);
+
+// Array.prototype.find()
+// Find is like filter, but instead returns just the one you are looking for
+// find the comment with the ID of 823423
+const check_ID = comments.find(comment => comment.id === 823423);
+console.log(check_ID);
+
+// Array.prototype.findIndex()
+// Find the comment with this ID
+// delete the comment with the ID of 823423
+const get_index = comments.findIndex(comment => comment.id === 823423);
+comments.splice(get_index,1); //first argument is position you want to delete from, second is how many you want to remove from the index you put it as first parameter in the bracket
+// console.log(comments);
