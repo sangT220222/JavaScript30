11db7feecc65fbede4f6ef9ddb4f00747fa9164a
sangT220222 <magnas9@hotmail.co.uk>
2024-03-23 09:16:08 +0000

    logic added so that drawing's starting and ending positons are corrected

--- Day 8/script.js
+++ Day 8/script.js
@@ -10,23 +10,28 @@ get_canvas.height = window.innerHeight;
 get_context.strokeStyle = 'red';
 get_context.lineJoin = 'round';
 get_context.lineCap = 'round';
+get_context.lineWidth = 50;
 
 let is_drawing = false; //flag for drawing based on mouse interaction
 
-let lastX = 0;//X,Y cordinates to know where you finish drawing
-let lastY = 0;
+let lastX ;//X,Y cordinates to know where you finish drawing
+let lastY ;
 
 function drawing(e){
     // console.log(e);
     if(!is_drawing) return;
-    console.log(e);
+    // console.log(e);
     get_context.beginPath();
     get_context.moveTo(lastX,lastY);//our starting position
     get_context.lineTo(e.offsetX,e.offsetY); //e.offset... is from the event that is given to us when the interactions happen
     get_context.stroke(); //this will begin teh drawing in the canvas
+    [lastX, lastY] = [e.offsetX,e.offsetY];
 }
 
 get_canvas.addEventListener('mousemove',drawing); //this gets whenever the mouse is moving on the canvas
-get_canvas.addEventListener('mousedown',() => is_drawing = true); 
+get_canvas.addEventListener('mousedown',(e) => {
+    is_drawing = true
+    [lastX, lastY] = [e.offsetX,e.offsetY]; //update lastX amd lastY instead of starting from 0 when mouse is down
+}); 
 get_canvas.addEventListener('mouseup',() => is_drawing = false); 
 get_canvas.addEventListener('mouseout',() => is_drawing = false); 
