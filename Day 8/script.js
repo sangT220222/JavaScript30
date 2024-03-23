51eb1def6a953fd0c100d2f485a968336fbb8588
sangT220222 <magnas9@hotmail.co.uk>
2024-03-23 09:09:13 +0000

    JS logic added to have something displayed on canvas when mouse is clicked and moved

---
+++ Day 8/script.js
@@ -0,0 +1,32 @@
+const get_canvas = document.querySelector('#draw');
+// context is what we use to draw on a canva
+const get_context = get_canvas.getContext('2d');
+
+
+// setting canvas' size to the window's
+get_canvas.width = window.innerWidth;
+get_canvas.height = window.innerHeight;
+
+get_context.strokeStyle = 'red';
+get_context.lineJoin = 'round';
+get_context.lineCap = 'round';
+
+let is_drawing = false; //flag for drawing based on mouse interaction
+
+let lastX = 0;//X,Y cordinates to know where you finish drawing
+let lastY = 0;
+
+function drawing(e){
+    // console.log(e);
+    if(!is_drawing) return;
+    console.log(e);
+    get_context.beginPath();
+    get_context.moveTo(lastX,lastY);//our starting position
+    get_context.lineTo(e.offsetX,e.offsetY); //e.offset... is from the event that is given to us when the interactions happen
+    get_context.stroke(); //this will begin teh drawing in the canvas
+}
+
+get_canvas.addEventListener('mousemove',drawing); //this gets whenever the mouse is moving on the canvas
+get_canvas.addEventListener('mousedown',() => is_drawing = true); 
+get_canvas.addEventListener('mouseup',() => is_drawing = false); 
+get_canvas.addEventListener('mouseout',() => is_drawing = false); 
