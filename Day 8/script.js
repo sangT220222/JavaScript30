b8e5269afca895bcaf00d73594f889daa1f27a23
sangT220222 <magnas9@hotmail.co.uk>
2024-03-23 09:31:34 +0000

    final changes for canvas implementation

--- Day 8/script.js
+++ Day 8/script.js
@@ -10,22 +10,41 @@ get_canvas.height = window.innerHeight;
 get_context.strokeStyle = 'red';
 get_context.lineJoin = 'round';
 get_context.lineCap = 'round';
-get_context.lineWidth = 50;
+get_context.lineWidth = 100;
 
 let is_drawing = false; //flag for drawing based on mouse interaction
 
-let lastX ;//X,Y cordinates to know where you finish drawing
-let lastY ;
+let lastX;//X,Y cordinates to know where you finish drawing
+let lastY;
+let hue = 0;
+let direction = true;
 
 function drawing(e){
     // console.log(e);
     if(!is_drawing) return;
     // console.log(e);
     get_context.beginPath();
+    get_context.strokeStyle = `hsl(${hue},100%,50%)`;
+    // get_context.lineWidth = hue;
     get_context.moveTo(lastX,lastY);//our starting position
     get_context.lineTo(e.offsetX,e.offsetY); //e.offset... is from the event that is given to us when the interactions happen
     get_context.stroke(); //this will begin teh drawing in the canvas
     [lastX, lastY] = [e.offsetX,e.offsetY];
+    hue ++; //hue is incremented as different values of hue give us different colours in the rainbow
+    if(hue === 360){
+        hue = 0;
+    }
+
+    if(get_context.lineWidth >= 100 || get_context.lineWidth <= 1){
+        direction = !direction;
+    }
+
+    if(direction){
+        get_context.lineWidth ++;
+    }
+    else{
+        get_context.lineWidth --;
+    }
 }
 
 get_canvas.addEventListener('mousemove',drawing); //this gets whenever the mouse is moving on the canvas
